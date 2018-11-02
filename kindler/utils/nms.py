8b9eb5fbb52a8b4c42c5d63ebeb8ca8d9b6b810a
mingruimingrui <mingrui.wang@shopee.com>
2018-11-02 19:36:08 +0800

    added comments

--- kindler/utils/nms.py
+++ kindler/utils/nms.py
@@ -1,5 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-# from ._utils import _C
 from .. import _C
 
+# NMS takes Tensors and applies non max supression
 nms = _C.nms
