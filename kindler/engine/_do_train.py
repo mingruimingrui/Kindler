4262cf73c1a5ad86bba4514a814061a11ebb60c9
mingruimingrui <mingrui.wang@shopee.com>
2018-11-04 19:46:15 +0800

    added do train

--- kindler/engine/_do_train.py
+++ kindler/engine/_do_train.py
@@ -1,9 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+
+import os
+import json
+import logging
+
 import time
 import datetime
-import logging
 
 from torch import distributed as dist
 
+from ..utils.metric_logger import MetricLogger
 from ..utils.misc import to_device
 from ..utils.comm import get_world_size, is_main_process
 
@@ -35,13 +42,31 @@ def reduce_loss_dict(loss_dict):
     return reduced_losses
 
 
+def save_model(checkpoint_dir, iter, model, optimizer, scheduler=None):
+    data = {
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+
+    if scheduler is not None:
+        data['scheduler'] = scheduler.state_dict()
+
+    if hasattr(model, 'config'):
+        data['config'] = model.config
+
+    file_name = os.path.join(checkpoint_dir, 'model_{}.pth.tar'.format(iter))
+    torch.save(data, file_name)
+
+
 def do_train(
     model,
     data_loader,
     loss_fn,
     optimizer,
     scheduler=None,
-    device=None,
+    logging_period=250,
+    checkpoint_period=2500,
+    checkpoint_dir='./'
 ):
     """
     Args:
@@ -54,13 +79,14 @@ def do_train(
             as the learning rate
         scheduler: The learning rate scheduler. If no scheduler is present,
             we assume constant learning rate
-        device: A torch.device object that model is on
     """
     logger.info('Start training')
     start_training_time = time.time()
 
     model.train()
+    device = next(model.parameters()).device
     max_iter = len(data_loader)
+    meters = MetricLogger(delimiter='  ')
 
     t0 = time.time()
     for iter, batch in enumerate(data_loader):
@@ -73,8 +99,9 @@ def do_train(
 
         loss_dict = loss_fn(model, batch)
 
-        # Comute reduced loss_dict
+        # Comute gather and reduce loss
         loss_dict_reduced = reduce_loss_dict(loss_dict)
+        meters.update(**loss_dict_reduced)
         reduce_time = time.time() - t0
 
         optimizer.zero_grad()
@@ -82,10 +109,29 @@ def do_train(
         optimizer.step()
 
         batch_time = time.time() - t0
+        meters.update(batch_time=batch_time, data_time=data_time, reduce_time=reduce_time)
         t0 = time.time()
 
-        time_elapsed = time.time() - start_training_time
-        eta_seconds = time_elapsed / iter * (max_iter - iter)
+        eta_seconds = meters.batch_time.global_avg / iter * (max_iter - iter)
         eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
 
-        
+        if iter % checkpoint_period == 0 and iter > 0:
+            save_model(checkpoint_dir, iter, model, optimizer, scheduler)
+
+        if iter % logging_period == 0 or iter == (max_iter - 1):
+            msg = {
+                'eta': eta_string,
+                'iter': iter,
+                'lr': '{:.6f}'.format(optimizer.param_groups[0]["lr"]),
+                'max mem': '{:.0f}'.format(torch.cuda.max_memory_allocated() / 1024.0 / 1024.0)
+            }
+            logger.info('{}'.format(msg))
+
+    save_model(checkpoint_dir, iter, model, optimizer, scheduler)
+    total_training_time = time.time() - start_training_time
+    total_time_str = str(datetime.timedelta(seconds=total_training_time))
+    msg = 'Total training time: {} ({:.4f}s / it)'.format(
+        total_time_str,
+        total_training_time / max_iter
+    )
+    logger.info(msg)
