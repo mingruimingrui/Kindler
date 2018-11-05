469cfe061986a0683397153d88da7abca7d08741
mingruimingrui <mingrui.wang@shopee.com>
2018-11-05 22:01:34 +0800

    changed to using dep distributed

--- kindler/utils/comm.py
+++ kindler/utils/comm.py
@@ -13,18 +13,18 @@ import torch
 
 
 def get_world_size():
-    if not torch.distributed.is_initialized():
+    if not torch.distributed.deprecated.is_initialized():
         return 1
-    return torch.distributed.get_world_size()
+    return torch.distributed.deprecated.get_world_size()
 
 
 def get_rank():
-    if not torch.distributed.is_initialized():
+    if not torch.distributed.deprecated.is_initialized():
         return 0
-    return torch.distributed.get_rank()
+    return torch.distributed.deprecated.get_rank()
 
 
 def is_main_process():
-    if not torch.distributed.is_initialized():
+    if not torch.distributed.deprecated.is_initialized():
         return True
-    return torch.distributed.get_rank() == 0
+    return torch.distributed.deprecated.get_rank() == 0
